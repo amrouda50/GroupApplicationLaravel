21a031375c7f659c89912baba5789fa4b108652e
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-08-08 04:07:37 +0200

    Group ui done

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="bg-slate-600 background" >
-        <div >
-            <NavBar v-bind:is-logged-in="true" v-bind:user-email="UserEmail" v-bind:user-name="UserName">
-
-            </NavBar>
-            <SideNavBar v-bind:groups="[...groups, ...groups, ...groups, ...groups]">
-
-            </SideNavBar>
-        </div>
-
+    <div class="bg-slate-600/80 background flex content">
+        <NavBar v-bind:is-logged-in="true" v-bind:user-email="UserEmail" v-bind:user-name="UserName" />
+        <SideNavBar
+            v-bind:groups="groups"
+            v-on:go-to-add-group="stats = 'addGroup'"
+        />
+        <AddGroup v-if="isAddGroup" />
+        <GroupDetails v-if="isGroupDetails" />
+        <UserDetails v-if="isUserDetails"/>
     </div>
 </template>
 
@@ -17,10 +16,17 @@
 <script>
 import NavBar from './nav/NavBar'
 import SideNavBar from './nav/SideNavBar'
+import AddGroup from './HomePageStates/AddGroup'
+import GroupDetails from './HomePageStates/GroupDetails'
+import UserDetails from './HomePageStates/UserDetails'
+
 export default {
     components:{
         SideNavBar,
         NavBar,
+        GroupDetails,
+        UserDetails,
+        AddGroup,
     },
     props:{
         UserName:{
@@ -37,12 +43,29 @@ export default {
    },
     data(){
       return{
-          groups:[]
+          groups:[],
+          stats:'default',
       }
     },
    async created() {
        const response = await fetch('./api/groups')
        this.groups = await response.json()
+    },
+    computed:{
+        isDefault(){
+            return this.stats === 'default'
+        },
+        isAddGroup(){
+            return this.stats === 'addGroup'
+        }
+        ,
+        isUserDetails(){
+            return this.stats === 'userDetails'
+
+        },
+        isGroupDetails(){
+            return this.stats === 'groupDetails'
+        }
     },
 }
 
---
+++ resources/js/Pages/HomePageStates/AddGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+
+        <form class="pl-20 pt-20">
+            <h1 class=" mb-6 text-xl font-bold text-white/80">Create Group</h1>
+            <div>
+                <label  class="text-white/80">Group name</label>
+                <input
+                    placeholder="Write the group name"
+                    class=" placeholder:text-sm form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-lg transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                />
+            </div>
+            <label  class="text-white/80">Group Description</label>
+
+            <div class=" mt-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+                <div class="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
+                    <textarea id="comment" rows="5" cols="60" class="px-0 outline-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write the group description" required></textarea>
+                </div>
+
+            </div>
+            <button type="button" class="mt-8 bg-blue-50	text-black py-2 px-4 text-sm flex-1 font-medium items-center text-white/60  rounded border border-white/60 hover:bg-white-100">
+                Add group
+            </button>
+        </form>
+
+    </div>
+</template>
+
+
+
+<script>
+
+
+</script>
+
+
+<style scoped>
+textarea{
+    resize: none;
+}
+</style>
