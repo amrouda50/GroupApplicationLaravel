2614bdd917c42bc38c76263be499e1e4f8b05f8b
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-08-10 01:37:42 +0200

    dragging users done

--- resources/js/Pages/nav/SideNavBar.vue
+++ resources/js/Pages/nav/SideNavBar.vue
@@ -34,6 +34,7 @@
                       v-bind:key="group.id"
                       v-on:drop="onDrop(index)"
                       v-on:dragover="onDragOver"
+                      class="cursor-move"
                   >
                       <button v-on:click="OpenGroup(index)" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                           <img v-on:click="DeleteGroup($event, index)"  class="h-5 w-5" :src="require('~/images/delete-icon.svg')" />
